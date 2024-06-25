70ce8d15075c37a1cc215c2207303be536e86c84
239007 <c239007@ivy.ac.jp>
2024-06-25 14:04:01 +0900

    ログイン画面追加

---
+++ login_test.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <title>ログイン画面</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic&display=swap');
+    </style>
+    <link rel="stylesheet" type="text/css" href="styles_S01.css">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+</head>
+
+<body>
+    <header>
+        <img src="" alt="Logo" class="logo">
+    </header>
+
+    <main>
+        <form id="loginForm" method="post" action="lotest.php"> <!-- PHPファイルへのアクションを指定 -->
+            <div class="form-group">
+                <label for="id">I D :</label>
+                <input type="text" id="id" name="id" required autofocus placeholder="ivy">
+            </div>
+            <div class="form-group">
+                <label for="pw">PW:</label>
+                <input type="password" id="pw" name="pw" required autofocus placeholder="1234">
+            </div>
+            <input type="submit" class="gradient1" value="ログイン">
+        </form>
+    </main>
+</body>
+</html>
---
+++ lotest.php
@@ -0,0 +1,38 @@
+<?php
+try{
+    $user_id = $_POST['id'];
+    $user_pass = $_POST['pw'];
+    $dsn = 'mysql:dbname=earnings;host=172.16.3.136;charset=utf8';
+    $db_username = "sample_user";
+    $db_password = "";
+
+    $dbh = new PDO($dsn, $db_username, $db_password);
+    $sql= "SELECT * FROM manager WHERE login_id = ? AND password = ?";
+    $stmt = $dbh->prepare($sql);
+    $data[]= $user_id;
+    $data[]= $user_pass;
+    $stmt->execute($data);
+
+    $dbh = null;
+
+    $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if($rec == false){
+        echo('IDまたはパスワードが間違っています。<br/>');
+        //header("Location: S01_login.php");
+    }else{
+        session_start();
+        $_SESSION['login'] = 1;
+        $_SESSION['db_username'] = $db_username;
+        $_SESSION['db_password'] = $db_password;
+        header("Location: main.php");
+        exit();
+    }
+}
+
+catch(Exception $e){
+    print('ただいまアクセスが集中しております。時間をおいて再度アクセスしてください。');
+    echo $e->getMessage();
+    exit();
+}
+?>
